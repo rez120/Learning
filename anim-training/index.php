b6d0dc219562982e7c40b27b9332fa37ae2ad64f
rez120 <r.pishkar120@gmail.com>
2021-07-24 00:26:46 +0430

    anim training

--- anim-training/index.php
+++ anim-training/index.php
@@ -1,6 +1,5 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="rtl">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -10,111 +9,55 @@
   </head>
   <body>
     <div class="container">
-      <div class="navbar">
-        <div class="header-right-section">
-          <div class="logo">
+      <header class="header">
+        <div class="header-content width-controller">
+          <div class="header-logo">
             <img
-              class="header-image"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Apple-logo.png/600px-Apple-logo.png"
+              class="header-logo-img"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Apple_logo_grey.svg/505px-Apple_logo_grey.svg.png"
+              alt="logo"
             />
           </div>
-          <div class="header-meta">
-            <div class="header-title">Lacanism</div>
-            <div class="header-description">Lorem ipsum dolores</div>
+          <div class="header-info">
+            <div class="header-title">عنوان</div>
+            <div class="header-description">
+              تکنولوژی آینده را اینجا پیدا کنید.
+            </div>
           </div>
-          <ul class="list">
-            <li class="item">
-              <div class="name">alpha</div>
-              <div class="bar"></div>
-            </li>
-            <li class="item">
-              <div class="name">bravo</div>
-              <div class="bar"></div>
-            </li>
-            <li class="item">
-              <div class="name">charlie</div>
-              <div class="bar"></div>
-            </li>
-            <li class="item">
-              <div class="name">delta</div>
-              <div class="bar"></div>
-            </li>
-            <li class="item">
-              <div class="name">echo</div>
-              <div class="bar"></div>
-            </li>
-            <li class="item">
-              <div class="name">foxtrot</div>
-              <div class="bar"></div>
-            </li>
+          <div class="header-burger">
+            <div class="burger">
+              <div class="burger-line"></div>
+              <div class="burger-line"></div>
+              <div class="burger-line"></div>
+            </div>
+          </div>
+          <ul class="header-navbar">
+            <li class="navbar-item">خانه</li>
+            <li class="navbar-item">مقالات</li>
+            <li class="navbar-item">درباره‌ ما</li>
+            <li class="navbar-item">ارتباط با ما</li>
           </ul>
-        </div>
-        <div class="header-search-section">
-          <form class="header-form" action="">
-            <input class="header-field" type="text" />
-            <input class="header-button" type="submit" />
+          <form class="header-search">
+            <input class="search-field" type="text" />
+            <input class="search-submit" type="submit" value="جستوجو" />
           </form>
         </div>
-      </div>
+      </header>
+      <div class="content">content</div>
+      <footer class="footer">footer</footer>
     </div>
 
     <script>
-      const navbar = document.querySelector(".container");
-      const list = document.querySelector(".list");
-      const items = document.querySelectorAll(".item");
-      const logo = document.querySelector(".logo");
-      window.addEventListener("scroll", () => {
-        if (window.scrollY > 0) {
-          navbar.classList.add("navbar2");
-          list.classList.add("list2");
-          logo.classList.add("logo-disappear");
-        } else {
-          if (list.classList.contains("list2")) {
-            list.classList.remove("list2");
-            navbar.classList.remove("navbar2");
-            logo.classList.remove("logo-disappear");
-          }
-        }
-      });
-      console.log(Array.from(items));
+      const burger = document.querySelector(".burger");
+      const header = document.querySelector(".header");
+      const headerContent = document.querySelector(".header-content");
+      console.log(headerContent.clientHeight);
+      burger.addEventListener("click", () => {
+        header.classList.toggle("header-toggle-active");
 
-      Array.from(items).forEach((e) => {
-        e.addEventListener("mouseover", () => {
-          const bar = e.querySelector(".bar");
-          bar.classList.add("bar-mouseover");
-        });
-      });
-
-      Array.from(items).forEach((e) => {
-        e.addEventListener("mouseout", () => {
-          const bar = e.querySelector(".bar");
-          bar.classList.remove("bar-mouseover");
-        });
-      });
-
-      // ------------------------
-
-      navbar.addEventListener("mouseover", () => {
-        if (navbar.classList.contains("navbar2")) {
-          navbar.classList.remove("navbar2");
-          list.classList.remove("list2");
-          logo.classList.remove("logo-disappear");
-        }
-      });
-
-      navbar.addEventListener("mouseout", () => {
-        if (!navbar.classList.contains("navbar2") && window.scrollY != 0) {
-          navbar.classList.add("navbar2");
-          list.classList.add("list2");
-          logo.classList.add("logo-disappear");
-        }
+        // header.style.height = "500px";
+        burger.classList.toggle("burger-toggle-active");
       });
     </script>
   </body>
 </html>
-
-
-<?php
-echo "hello world";
-
-?>
