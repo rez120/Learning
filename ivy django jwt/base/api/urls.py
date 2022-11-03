200299c21d5b18ccc2ed4bfc702f7425c8cd7e8a
reza <r.pishkar121@gmail.com>
2022-11-03 11:29:44 +0330

    finished jwt basics

--- ivy django jwt/base/api/urls.py
+++ ivy django jwt/base/api/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('token/', views.MyTokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     
     path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
+    
+    path('notes', views.getNotes),
 ]
--- ivy django jwt/base/api/views.py
+++ ivy django jwt/base/api/views.py
@@ -1,9 +1,15 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+
+from .serializers import NoteSerializer
+
+from base.models import Note
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -27,3 +33,12 @@ def getRoutes(request):
         '/api/token/refresh',
     ]
     return Response(routes)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many = True)
+    return Response(serializer.data)
